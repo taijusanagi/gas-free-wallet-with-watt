d14e248893ea606311c1a3e1a5beda7806040cab
taijusanagi <taijusanagi@gmail.com>
2023-10-12 16:10:32 +0700

    add watt validation

--- contracts/contracts/WattPaymaster.sol
+++ contracts/contracts/WattPaymaster.sol
@@ -5,12 +5,22 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 import "@account-abstraction/contracts/core/BasePaymaster.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract WattPaymaster is BasePaymaster {
 
+    // WATTContract
+    IERC20 public watt;
+    uint256 public threshold;
+
     constructor(
-        IEntryPoint _entryPoint
-    ) BasePaymaster(_entryPoint) {}
+        IEntryPoint _entryPoint,
+        IERC20 _watt,
+        uint256 _threshold
+    ) BasePaymaster(_entryPoint) {
+        watt = _watt;
+        threshold = _threshold;
+    }
 
     function _validatePaymasterUserOp(
         UserOperation calldata userOp,
@@ -23,7 +33,9 @@ contract WattPaymaster is BasePaymaster {
         returns (bytes memory context, uint256 validationData)
     {
         // validate WATT amount
-
+        // https://developer.newcoin.org/docs/getting-started/using-watts/querying-watts/
+        uint256 balance = watt.balanceOf(userOp.sender);
+        require(balance >= threshold, "watt balance is not enough");
         return (abi.encode(), 0);
     }
 
