4f5648c12d07fc56b0d60b8b9f9d737793e0bcad
taijusanagi <taijusanagi@gmail.com>
2023-10-12 17:03:21 +0700

    add wallet

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,9 +1,16 @@
 import { Cormorant } from "next/font/google";
 import { useState } from "react";
+import { useIsConnected } from "@/hooks/useIsConnected";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
 
 const cormorant = Cormorant({ subsets: ["latin"] });
 
 export default function Home() {
+  const { isConnected } = useIsConnected();
+  const { openConnectModal } = useConnectModal();
+  const { address } = useAccount();
+
   const [accountAbstractionWaleltAddress, setAccountAbstractionWaleltAddress] = useState("");
   const [ethBalance, setETHTBalance] = useState("0");
   const [wattBalance, setWATTBalance] = useState("0");
@@ -19,50 +26,67 @@ export default function Home() {
   return (
     <div className={`min-h-screen bg-white text-gray-800 ${cormorant.className}`}>
       <header className="py-4 px-8 flex justify-end items-center">
-        <button className="bg-white border-gray-500 border hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-gray">
-          Connect Wallet
-        </button>
+        {!isConnected && (
+          <button
+            className="bg-white border-gray-500 border hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-gray"
+            onClick={openConnectModal}
+          >
+            Connect Wallet
+          </button>
+        )}
       </header>
-      <main className="flex flex-col items-center justify-center h-full py-24">
+      <main className="flex flex-col items-center justify-center h-full py-32">
         <section className="w-full max-w-md mb-8">
           <h1 className="text-3xl font-bold mb-2 text-center">WATT Paymaster</h1>
           <h1 className="text-md font-bold text-center">Gas fee subsidise with WATT tokens</h1>
         </section>
         <section className="w-full max-w-md">
-          <form className="bg-gray-100 p-4 rounded shadow-md">
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold">Connected Wallet</label>
-              <p className="text-gray-700 text-xs">p</p>
+          {!isConnected && (
+            <div className="flex justify-center">
+              <button
+                className="bg-white border-gray-500 border hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-gray"
+                onClick={openConnectModal}
+              >
+                Connect Wallet
+              </button>
             </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold">Account Abstraction Wallet</label>
-              <p className="text-gray-700 text-xs">{accountAbstractionWaleltAddress}</p>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold">ETH Balance</label>
-              <p className="text-gray-700 text-xs">{ethBalance} ETH</p>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold">WATT Balance</label>
-              <p className="text-gray-700 text-xs">{wattBalance} WATT</p>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2">Connect dApps with Wallet Connect</label>
-              <input
-                className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xs"
-                type="text"
-                placeholder="wc:"
-                value={walletConenctURI}
-                onChange={(e) => setWalletConnectURI(e.target.value)}
-              />
-            </div>
-            <button
-              className="w-full bg-white border-gray-500 border hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-gray"
-              type="button"
-            >
-              Connect Wallet
-            </button>
-          </form>
+          )}
+          {isConnected && (
+            <form className="bg-gray-100 p-4 rounded shadow-md">
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold">Connected Wallet</label>
+                <p className="text-gray-700 text-xs">{address}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold">Account Abstraction Wallet</label>
+                <p className="text-gray-700 text-xs">{accountAbstractionWaleltAddress}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold">ETH Balance</label>
+                <p className="text-gray-700 text-xs">{ethBalance} ETH</p>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold">WATT Balance</label>
+                <p className="text-gray-700 text-xs">{wattBalance} WATT</p>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">Connect dApps with Wallet Connect</label>
+                <input
+                  className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xs"
+                  type="text"
+                  placeholder="wc:"
+                  value={walletConenctURI}
+                  onChange={(e) => setWalletConnectURI(e.target.value)}
+                />
+              </div>
+              <button
+                className="w-full bg-white border-gray-500 border hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-gray"
+                type="button"
+              >
+                Connect dApps
+              </button>
+            </form>
+          )}
         </section>
       </main>
       {isModalOpen && (
